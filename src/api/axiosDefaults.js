c9a834b8529136ac16c549bf6869ad6785fe4588
Frida Wikell <fridasvensson7@gmail.com>
2024-04-17 10:28:23 +0000

    Add: Use memo to refresh tokens in user context

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://greenthumb-back-4bd145d8f205.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
