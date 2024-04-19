d83db97913ae8aef6d7daf268b66fd014ebc4783
Frida Wikell <fridasvensson7@gmail.com>
2024-04-19 08:10:44 +0000

    Add: Modal to tell the user they have already voted

--- src/pages/polls/VoteForm.js
+++ src/pages/polls/VoteForm.js
@@ -1,10 +1,14 @@
 // VoteForm.js
-import React from "react";
+import React, { useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import btnStyles from "../../styles/Button.module.css";
 
 function VoteForm({ questionId, selectedAnswerId }) {
+  const [showModal, setShowModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const handleSubmit = async () => {
     if (!selectedAnswerId) {
       alert("Please select an answer before voting.");
@@ -17,8 +21,14 @@ function VoteForm({ questionId, selectedAnswerId }) {
       // No need to reset selectedAnswerId here, handle that in the QuestionPage if needed
     } catch (err) {
       console.error("Error submitting vote:", err);
+      if (err.response && err.response.data) {
+        setErrorMessage("You have already voted on this question."); // Customize based on your API response
+        setShowModal(true);
+      }
     }
   };
+
+  const handleClose = () => setShowModal(false);
 
   return (
     <div>
@@ -29,6 +39,17 @@ function VoteForm({ questionId, selectedAnswerId }) {
       >
         Submit Vote
       </Button>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Voting Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{errorMessage}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
