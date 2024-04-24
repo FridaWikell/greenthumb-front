e47ad11e024affa524f3a783d531041512519ca7
Frida Wikell <fridasvensson7@gmail.com>
2024-04-24 09:13:59 +0000

    Add: New spinner when pages is loading

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Spinner from "react-bootstrap/Spinner";
+import Lottie from "react-lottie";
+import spinnerAnimation from "./Spinner.json";
 import styles from "../styles/Asset.module.css";
 
 const Asset = ({ spinner, src, message }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: spinnerAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   return (
     <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
+      {spinner && <Lottie options={defaultOptions} height={150} width={150} />}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
     </div>
