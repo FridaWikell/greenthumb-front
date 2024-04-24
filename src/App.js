f91f5f777dce4625c34dd6b817a877285da5bed7
Frida Wikell <fridasvensson7@gmail.com>
2024-04-24 15:18:15 +0000

    Add: Error boundary to catch errors and redirect to 500 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
@@ -18,7 +19,6 @@ import Questions from "./pages/polls/Questions";
 import QuestionForm from "./pages/polls/QuestionForm";
 import QuestionPage from "./pages/polls/QuestionPage";
 import NotFoundPage from "./pages/errors/NotFoundPage";
-import InternalServerErrorPage from "./pages/errors/InternalServerErrorPage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -28,55 +28,56 @@ function App() {
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <Switch>
-        <Route
-            exact path="/"
-            render={() => (
-              <PostsPage message="No results found. Adjust the search keyword." />
-            )}
-          />
-          <Route
-            exact path="/feed"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or follow a user."
-                filter={`owner__followed__owner__profile=${profile_id}&`}
-              />
-            )}
-          />
-          <Route
-            exact path="/liked"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or like a post."
-                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-              />
-            )}
-          />
-          <Route exact path="/signin" render={() => <SignInForm />} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
-          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-          <Route exact path="/posts/:id" render={() => <PostPage />} />
-          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
-          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
-          <Route 
-          exact path="/profiles/:id/edit/username"
-          render={() => <UsernameForm />}
-          />
-          <Route
-            exact path="/profiles/:id/edit/password"
-            render={() => <UserPasswordForm />}
-          />
-          <Route
-            exact path="/profiles/:id/edit"
-            render={() => <ProfileEditForm />}
-          />
-          <Route exact path="/questions" render={() => <Questions />} />
-          <Route exact path="/questions/create" render={() => <QuestionForm />} />
-          <Route exact path="/questions/:id" render={() => <QuestionPage />} />
-          <Route exact path="/work" render={() => <InternalServerErrorPage />} />
-          <Route render={() => <NotFoundPage />} />
-        </Switch>
+        <ErrorBoundary>
+          <Switch>
+            <Route
+              exact path="/"
+              render={() => (
+                <PostsPage message="No results found. Adjust the search keyword." />
+              )}
+            />
+            <Route
+              exact path="/feed"
+              render={() => (
+                <PostsPage
+                  message="No results found. Adjust the search keyword or follow a user."
+                  filter={`owner__followed__owner__profile=${profile_id}&`}
+                />
+              )}
+            />
+            <Route
+              exact path="/liked"
+              render={() => (
+                <PostsPage
+                  message="No results found. Adjust the search keyword or like a post."
+                  filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                />
+              )}
+            />
+            <Route exact path="/signin" render={() => <SignInForm />} />
+            <Route exact path="/signup" render={() => <SignUpForm />} />
+            <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+            <Route exact path="/posts/:id" render={() => <PostPage />} />
+            <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+            <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+            <Route 
+            exact path="/profiles/:id/edit/username"
+            render={() => <UsernameForm />}
+            />
+            <Route
+              exact path="/profiles/:id/edit/password"
+              render={() => <UserPasswordForm />}
+            />
+            <Route
+              exact path="/profiles/:id/edit"
+              render={() => <ProfileEditForm />}
+            />
+            <Route exact path="/questions" render={() => <Questions />} />
+            <Route exact path="/questions/create" render={() => <QuestionForm />} />
+            <Route exact path="/questions/:id" render={() => <QuestionPage />} />
+            <Route render={() => <NotFoundPage />} />
+          </Switch>
+        </ErrorBoundary>
       </Container>
     </div>
   );
--- src/pages/errors/InternalServerErrorPage.js
+++ src/pages/errors/InternalServerErrorPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
@@ -9,26 +8,15 @@ import Col from "react-bootstrap/Col";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/NotFoundPage.module.css";
-import btnStyles from "../../styles/Button.module.css";
 
 const InternalServerErrorPage = () => {
-  const history = useHistory();
-
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2 mx-auto" lg={8}>
         <div className="d-flex justify-content-center mb-4">
           <Container className={`mb-4 py-4 px-3 px-md-5 ${appStyles.Content}`}>
             <h1 className={`${styles.Header} mt-2`}>Oh dear! Our garden seems a bit overgrown...</h1>
-            <p>It looks like our system might have tangled itself up in the vines. We're tending to it as quickly as we can, but feel free to prune back to the previous page or dig your way home and try again later.</p>
-            <div className="d-flex justify-content-center my-4">
-              <Button className={`${btnStyles.StandardBtn} px-3 py-2 mx-2`} onClick={() => history.goBack()}>
-                Go back
-              </Button>
-              <Button className={`${btnStyles.StandardBtn} px-3 py-2 mx-2`} onClick={() => history.push('/')}>
-                Home page
-              </Button>
-            </div>
+            <p>It looks like our system might have tangled itself up in the vines. We're tending to it as quickly as we can, but feel free to try again later.</p>
             <div className="d-flex justify-content-center">
               <Image src="https://res.cloudinary.com/dihkuau3v/image/upload/v1713969505/500-page_robqzq.webp" alt="Happy plant" fluid />
             </div>
