c54272bb23a005f3f1d1e5395139e89a6af99cf0
Frida Wikell <fridasvensson7@gmail.com>
2024-04-26 13:04:59 +0000

    Fix: Take back hardiness zone dropdown and change back image position

--- src/pages/posts/PostEditForm.js
+++ src/pages/posts/PostEditForm.js
@@ -35,7 +35,7 @@ const PostEditForm = () => {
             title: data.title,
             content: data.content,
             image: data.image,
-            hardinessZone: data.hardiness_zone,
+            hardinessZone: data.hardiness_zone, // Convert backend snake_case to camelCase
           });
         } else {
           history.push("/");
@@ -82,7 +82,6 @@ const PostEditForm = () => {
       await axiosReq.put(`/posts/${id}/`, formData);
       history.push(`/posts/${id}`);
     } catch (err) {
-      // console.error("Failed to update post:", err);
       if (err.response && err.response.status !== 401) {
         setErrors(err.response.data);
       }
@@ -94,12 +93,30 @@ const PostEditForm = () => {
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
-            <Form.Group className="text-center">
-              <figure><Image className={appStyles.Image} src={image} rounded /></figure>
-              <Form.Label className={`${btnStyles.Button} ${btnStyles.Blue} btn`} htmlFor="image-upload">Change the image</Form.Label>
-              <Form.File id="image-upload" accept="image/*" onChange={handleChangeImage} ref={imageInput} />
+          <Form.Group className="text-center">
+              <figure>
+                <Image className={appStyles.Image} src={image} rounded />
+              </figure>
+              <div>
+                <Form.Label
+                  className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                  htmlFor="image-upload"
+                >
+                  Change the image
+                </Form.Label>
+              </div>
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
             </Form.Group>
-            {errors.image?.map((message, idx) => <Alert key={idx} variant="warning">{message}</Alert>)}
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
           </Container>
         </Col>
         <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
@@ -116,14 +133,29 @@ const PostEditForm = () => {
                 {errors.content?.map((error, idx) => <Alert key={idx} variant="warning">{error}</Alert>)}
               </Form.Group>
               <Form.Group>
-                <Form.Label>Hardiness zone</Form.Label>
-                <Form.Control as="select" name="hardinessZone" value={hardinessZone} onChange={handleChange}>
-                  {Array.from({ length: 9 }, (_, index) => index).map(zone => (
-                    <option key={zone} value={zone}>{`Zone ${zone}`}</option>
-                  ))}
+                <Form.Label>Hardiness Zone</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="hardinessZone"
+                  value={hardinessZone}
+                  onChange={handleChange}
+                >
+                  <option value="0">Not applicable</option>
+                  <option value="1">Zone 1</option>
+                  <option value="2">Zone 2</option>
+                  <option value="3">Zone 3</option>
+                  <option value="4">Zone 4</option>
+                  <option value="5">Zone 5</option>
+                  <option value="6">Zone 6</option>
+                  <option value="7">Zone 7</option>
+                  <option value="8">Zone 8</option>
                 </Form.Control>
-                {errors.hardinessZone?.map((error, idx) => <Alert key={idx} variant="warning">{error}</Alert>)}
               </Form.Group>
+              {errors.hardinessZone?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
               <Button className={`${btnStyles.CancelBtn} px-3 py-2 mt-3 mx-2`} onClick={() => history.goBack()}>Cancel</Button>
               <Button className={`${btnStyles.SubmitBtn} px-3 py-2 mt-3 mx-2`} type="submit">Save</Button>
             </div>
